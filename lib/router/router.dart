0b394e7b67e6aa765736fb64ef533f8520cbc102
ArtemGusevGit <artemdvd@gmail.com>
2026-01-12 13:54:22 +0300

    refactor: simplify CryptoListBloc initialization in router

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:test_app/repositories/crypto_coins/crypto_coins_repository.dart';
 
 import '../features/crypto_list/block/crypto_list_bloc.dart';
 import '../features/crypto_list/view/view.dart';
@@ -11,8 +12,17 @@ import '../repositories/single_coin/abstract_single_coin_repository.dart';
 
 final Map<String, WidgetBuilder> router = {
   '/': (BuildContext context) => BlocProvider(
-    create: (_) => CryptoListBloc(GetIt.I<AbstractCoinsRepository>())
-      ..add(LoadCryptoList()), child: const CryptoListScreen()),
+    create: (_) {
+      final bloc = CryptoListBloc(
+        GetIt.I<AbstractCoinsRepository>(),
+      );
+
+      bloc.add(LoadCryptoList());
+
+      return bloc;
+    },
+    child: const CryptoListScreen(),
+  ),
   '/coin': (BuildContext context) {
     final args = ModalRoute.of(context)?.settings.arguments as Map?;
     final coinSymbol = args?['coin'] as String?;
